ed5adf142b6d04f21f91a22e0cd83a86aa317ed4
sboardwell <sboardwell@cloudbees.com>
2025-03-11 07:43:39 +0100

    Rename test

--- bundleutilspkg/tests/test_bundleutils.py
+++ bundleutilspkg/tests/test_bundleutils.py
@@ -11,12 +11,12 @@ yaml = ruamel.yaml.YAML(typ="rt")
 def runner():
     return CliRunner()
 
-def test_hello_command(runner):
+def test_usage(runner):
     result = runner.invoke(bundleutils, [])
     assert result.exit_code == 0
     assert "Usage: bundleutils [OPTIONS]" in result.output
 
-def test_hello_command_missing_argument(runner):
+def test_completion_missing_argument(runner):
     result = runner.invoke(bundleutils, ["completion"])
     assert result.exit_code != 0  # Click should return an error
     assert "Missing option '-s'" in result.output
