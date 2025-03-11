8b134a8874b7f9d08852fe2ab9e14adb1c3aaf2c
sboardwell <sboardwell@cloudbees.com>
2025-03-11 09:39:48 +0100

    Add tests

--- bundleutilspkg/tests/test_bundleutils.py
+++ bundleutilspkg/tests/test_bundleutils.py
@@ -21,18 +21,17 @@ def test_completion_missing_argument(runner):
     assert result.exit_code != 0  # Click should return an error
     assert "Missing option '-s'" in result.output
 
-def test_merge_bundles_1(request, runner):
-    testdir = os.path.dirname(__file__)
-    outdir = os.path.join(testdir, 'resources', 'target', request.node.name)
-    assert outdir.endswith('tests/resources/target/test_merge_bundles_1')
+def _test_merge_bundles(testdir, test_name, runner, command_args, expected_dir):
+    outdir = os.path.join(testdir, 'resources', 'target', test_name)
+    assert outdir.endswith(f'tests/resources/target/{test_name}')
     # delete the output directory recursively if it exists
     shutil.rmtree(outdir, ignore_errors=True)
     os.makedirs(outdir, exist_ok=True)
-    result = runner.invoke(bundleutils, ["merge-bundles", "-b", f"{testdir}/resources/merge-bundles/base", "-b", f"{testdir}/resources/merge-bundles/child1", "-o", outdir])
+    result = runner.invoke(bundleutils, command_args)
     assert result.exit_code == 0
     assert os.path.exists(os.path.join(outdir, 'bundle.yaml'))
     # compare the yaml files in the output directory with the equivalent yaml files in the expected directory
-    expected_dir = os.path.join(os.path.dirname(__file__), 'resources/merge-bundles/base-child1-expected')
+    expected_dir = os.path.join(os.path.dirname(__file__), expected_dir)
     for file in os.listdir(outdir):
         # ignore the bundle.yaml file
         if file == 'bundle.yaml':
@@ -42,3 +41,38 @@ def test_merge_bundles_1(request, runner):
         with open(os.path.join(expected_dir, file), 'r') as f:
             expected_data = yaml.load(f)
         assert outdata == expected_data
+
+
+def test_merge_bundles_base_only(request, runner):
+    testdir = os.path.dirname(__file__)
+    test_name = request.node.name
+    outdir = os.path.join(testdir, 'resources', 'target', test_name)
+    expected_dir = 'resources/merge-bundles/base-expected'
+    command_args = ["merge-bundles",
+                    "-b", f"{testdir}/resources/merge-bundles/base",
+                    "-o", outdir]
+    _test_merge_bundles(testdir, test_name, runner, command_args, expected_dir)
+
+def test_merge_bundles_base_child(request, runner):
+    testdir = os.path.dirname(__file__)
+    test_name = request.node.name
+    outdir = os.path.join(testdir, 'resources', 'target', test_name)
+    expected_dir = 'resources/merge-bundles/base-child1-expected'
+    command_args = ["merge-bundles",
+                    "-b", f"{testdir}/resources/merge-bundles/base",
+                    "-b", f"{testdir}/resources/merge-bundles/child1",
+                    "-o", outdir]
+    _test_merge_bundles(testdir, test_name, runner, command_args, expected_dir)
+
+def test_merge_bundles_base_child_grandchild(request, runner):
+    testdir = os.path.dirname(__file__)
+    test_name = request.node.name
+    outdir = os.path.join(testdir, 'resources', 'target', test_name)
+    expected_dir = 'resources/merge-bundles/base-child1-grandchild1-expected'
+    command_args = ["merge-bundles",
+                    "-b", f"{testdir}/resources/merge-bundles/base",
+                    "-b", f"{testdir}/resources/merge-bundles/child1",
+                    "-b", f"{testdir}/resources/merge-bundles/grandchild1",
+                    "-o", outdir]
+    _test_merge_bundles(testdir, test_name, runner, command_args, expected_dir)
+
