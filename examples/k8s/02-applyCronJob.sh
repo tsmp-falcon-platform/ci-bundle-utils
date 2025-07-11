b056534dac1f2979473a0a3fc0e27275ee788725
cccaternberg <acaternberg@cloudbees.com>
2025-07-11 16:07:23 +0200

    update ssh key scan

--- examples/k8s/02-applyCronJob.sh
+++ examples/k8s/02-applyCronJob.sh
@@ -12,7 +12,7 @@ kubectl delete -f "$YAML_FILE" --ignore-not-found
 kubectl apply -f "$YAML_FILE"
 
 echo "⏳ Waiting for job to be scheduled..."
-echo "🔍 Wait some seconds, until the first job is started, the run ./03-readJobLogs.sh"
+echo "🔍 Wait some seconds, until the first job is started, the run ./03-readJobLogs.sh -n $NAMESPACE"
 #sleep 130
 #echo "🔍 Finding the most recent job created by the CronJob..."
 #kubectl logs -f $(kubectl get pods --selector=job-name=$(kubectl get jobs --sort-by=.metadata.creationTimestamp | grep $CRONJOB_NAME  | head -n 1 | awk '{print $1}') -o jsonpath='{.items[0].metadata.name}')
