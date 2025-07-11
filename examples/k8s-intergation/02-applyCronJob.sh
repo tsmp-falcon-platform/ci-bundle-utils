7e7378c2861e060d48b5782f5e543a69eb1444e6
cccaternberg <acaternberg@cloudbees.com>
2025-07-11 13:45:07 +0200

    add namespace support

--- examples/k8s-intergation/02-applyCronJob.sh
+++ examples/k8s-intergation/02-applyCronJob.sh
@@ -12,9 +12,7 @@ kubectl delete -f "$YAML_FILE" --ignore-not-found
 kubectl apply -f "$YAML_FILE"
 
 echo "⏳ Waiting for job to be scheduled..."
-# Wartezeit zur Ausführung des CronJobs (je nach .spec.schedule & .startingDeadlineSeconds ggf. anpassen)
-
-
+echo "🔍 Wait some seconds, until the first job is started, the run ./03-readJobLogs.sh"
 #sleep 130
 #echo "🔍 Finding the most recent job created by the CronJob..."
 #kubectl logs -f $(kubectl get pods --selector=job-name=$(kubectl get jobs --sort-by=.metadata.creationTimestamp | grep $CRONJOB_NAME  | head -n 1 | awk '{print $1}') -o jsonpath='{.items[0].metadata.name}')
